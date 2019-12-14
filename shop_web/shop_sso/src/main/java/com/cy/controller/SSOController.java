d62b36cd1d20a2a4cdd977a8647469c78d8650da
PatyQ <qxy136181378@gmail.com>
2019-12-14 09:10:06 +0800

    自定义注解之前

--- shop_web/shop_sso/src/main/java/com/cy/controller/SSOController.java
+++ shop_web/shop_sso/src/main/java/com/cy/controller/SSOController.java
@@ -1,12 +1,16 @@
 package com.cy.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.cy.entity.ResultData;
 import com.cy.entity.User;
 import com.cy.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -55,7 +59,7 @@ public class SSOController {
      * @return
      */
     @RequestMapping(value = "login")
-    public String login(String username, String password, HttpServletResponse response){
+    public String login(String username, String password, String returnUrl, HttpServletResponse response){
         User user = userService.selUserByName(username);
         if (user==null){
             return "login";
@@ -75,9 +79,60 @@ public class SSOController {
         cookie.setDomain("localhost");//域名
         cookie.setPath("/");
 
+        System.out.println("将要返回的页面"+returnUrl);
         response.addCookie(cookie);
-        return "redirect:http://localhost:8889";
+        if (returnUrl==null || returnUrl.equals("")){
+            return "redirect:http://localhost:8889";
+        }else {
+            return "redirect:"+returnUrl;
+        }
     }
 
+    /**
+     * 判断是否登录
+     * @param loginToken
+     * @param callback
+     * @return
+     */
+    @RequestMapping(value = "islogin")
+    @ResponseBody
+    //jsonp格式需要,gosn拼接callback返回
+    public String islogin(@CookieValue(value = "loginToken" ,required = false) String loginToken, String callback){
+        System.out.println("获取到的token值"+loginToken);
+        System.out.println("获取到的callback值"+callback);//获取到的token值null
+        ResultData resultData = new ResultData();
+//        Gson gson = new Gson();
+        if (loginToken!=null){
+            User user = (User) redisTemplate.opsForValue().get(loginToken);
+            if (user!=null){
+                resultData.setCode("200").setMsg("成功").setData(user);
+            }
+        }else {
+            resultData.setCode("500");
+        }
+        //可以判断callback是否为空,空则为ajax请求
+        return callback+"("+ JSON.toJSONString(resultData) +")";
+    }
+
+    /**
+     * 注销用户返回登录首页
+     *删除redis中的令牌,移除cookie
+     */
+    @RequestMapping(value = "logout")
+    public String userLogout(@CookieValue(name = "loginToken",required=false)String loginToken,HttpServletResponse response){
+        redisTemplate.delete(loginToken);
+        Cookie cookie = new Cookie("loginToken",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:http://localhost:8889/";
+    }
+
+    /**
+     * 注册用户
+     */
+    @RequestMapping(value = "toregist")
+    public String toregist(){
+        return "redirect:http://localhost:8889/";
+    }
 
 }
